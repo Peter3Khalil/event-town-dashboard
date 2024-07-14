fb2243cec7e791332bea27db47d01b8e1e841075
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-07-15 02:57:41 +0300

    ♻️ Refactoring the Layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,26 +1,10 @@
 'use client';
-import { Page, PageLayout, PageSection } from '@/components/layouts/PageLayout';
-import MobileHeader from '@/components/MobileHeader';
+import MainLayout from '@/components/layouts/MainLayout';
 import PrivateRoute from '@/components/PrivateRoute';
-import CustomBreadcrumb from '@/components/shared/CustomBreadcrumb';
 import Sidebar from '@/components/Sidebar';
 import { AuthProvider } from '@/providers/auth-provider';
-import {
-  BreadcrumbProvider,
-  useBreadcrumb,
-} from '@/providers/breadcrumb-provider';
+import { BreadcrumbProvider } from '@/providers/breadcrumb-provider';
 import React from 'react';
-
-const BreadcrumbWrapper = () => {
-  const { breadcrumbItems, breadcrumbPage } = useBreadcrumb();
-
-  return (
-    <CustomBreadcrumb
-      breadcrumbItems={breadcrumbItems}
-      breadcrumbPage={breadcrumbPage}
-    />
-  );
-};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -29,17 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <PrivateRoute>
           <div className="flex h-svh w-full">
             <Sidebar className="shrink-0" />
-            <main className="h-full w-full overflow-auto pb-4 sm:px-4 sm:py-4 sm:pb-2">
-              <PageLayout className="gap-2 pt-0 sm:gap-4">
-                <MobileHeader />
-                <Page className="h-[calc(100%-3.5rem)] px-6 sm:h-full">
-                  <PageSection>
-                    <BreadcrumbWrapper />
-                  </PageSection>
-                  {children}
-                </Page>
-              </PageLayout>
-            </main>
+            <MainLayout>{children}</MainLayout>
           </div>
         </PrivateRoute>
       </BreadcrumbProvider>
