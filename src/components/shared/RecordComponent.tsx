69da06ba0b1c840aa5a295e815ce3b0757157d60
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-07-30 17:48:09 +0300

    🚑 Styling: Preventing Displaying Empty record

--- src/components/shared/RecordComponent.tsx
+++ src/components/shared/RecordComponent.tsx
@@ -5,23 +5,19 @@ interface RecordProps extends React.HTMLAttributes<HTMLParagraphElement> {
   label: string;
   value: string | number;
 }
-const RecordComponent = ({
-  label,
-  value,
-  className,
-  ...props
-}: RecordProps) => (
-  <p
-    className={cn(
-      'w-full overflow-hidden text-ellipsis text-[0.9em] text-muted-foreground',
-      className,
-    )}
-    title={value?.toString()}
-    {...props}
-  >
-    <span className="mr-1 capitalize">{label}: </span>
-    <span className="font-medium">{value}</span>
-  </p>
-);
+const RecordComponent = ({ label, value, className, ...props }: RecordProps) =>
+  value ? (
+    <p
+      className={cn(
+        'w-full overflow-hidden text-ellipsis text-[0.9em] text-muted-foreground',
+        className,
+      )}
+      title={value?.toString()}
+      {...props}
+    >
+      <span className="mr-1 capitalize">{label}: </span>
+      <span className="font-medium">{value}</span>
+    </p>
+  ) : null;
 
 export default RecordComponent;
