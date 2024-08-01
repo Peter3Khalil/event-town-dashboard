29ca8cdd4616c570db0e9b8bb4ed9d0149408819
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-08-01 22:36:12 +0300

    ➖ Removing unused utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { Toast, ToasterToast } from '@/components/ui/use-toast';
-import { isAxiosError } from 'axios';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -9,29 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDateTime = (date: string) => {
   return new Date(date).toLocaleString();
-};
-
-export const toastError = (
-  error: unknown,
-  // eslint-disable-next-line no-unused-vars
-  toast: ({ ...props }: Toast) => {
-    id: string;
-    dismiss: () => void;
-    // eslint-disable-next-line no-unused-vars
-    update: (props: ToasterToast) => void;
-  },
-) => {
-  if (isAxiosError(error)) {
-    return toast({
-      title: 'Error',
-      description: error.response?.data.message,
-    });
-  }
-
-  toast({
-    title: 'Error',
-    description: 'An error occurred while rejecting the event',
-  });
 };
 
 export const isClient = () => typeof window !== 'undefined';
